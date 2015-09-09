2bcfab434ba596dbf306e178947e54d3f9715cfb
Nikolas Rassoules <nikolas.rassoules@gmail.com>
2015-09-09 19:01:59 -0400

    added hover animations and topic buttons

--- index.php
+++ index.php
@@ -8,23 +8,46 @@
 	<body>
 		<section id="imgSection">
 			<section id="fillSection">
-				<h1 class="bringToFront">Changing Currents</h1>
+				<h1 class="bringToFront"><a href="http://www.thevoicesoftheants.com">Changing Currents</a></h1>
 				<div class="jumbotron" id="frontPage">
 					<div class="container" id="bar">
 						<div id="redCircle"></div>
 						<div id="yellowCircle"></div>
 						<div id="greenCircle"></div>
 					</div>
-					<h2 class="currentsText">Topics</h2>
+					<div id="triggerHide">
+						<h2 class="currentsText">Topics</h2>
+						<div class="well currentsWell">
+							<div class="well currentsWell hvr-radial-out topicButtonFirst">
+								<h2>Fashion</h2>
+							</div> 
+							<div class="well currentsWell hvr-radial-out topicButton">
+								<h2>Hip Hop</h2>
+							</div>
+							<div class="well currentsWell hvr-radial-out topicButton">
+								<h2>Political</h2>
+							</div>
+							<div class="well currentsWell hvr-radial-out topicButton">
+								<h2>Ideas</h2>
+							</div>
+							<div class="well currentsWell hvr-radial-out topicButton">
+								<h2>Photography</h2>
+							</div>
+						</div>
+					</div>
+					<h2 class="currentsText black trigger">Top Post Across All Topics:</h2>
 				</div>
 			</section>
 		</section>
-		
 	</body>
 	<script type="text/Javascript">
 			$(document).ready(function(){
 				$(".bringToFront").hide().fadeIn(2000);
 				$(".bringToFront").animate({marginTop : "70px"} , 1000);
+
+				$(".trigger").click(function(){
+					$("#triggerHide").fadeOut(1000);
+				});
 			});
 	</script>
 </html>
--- views/links.php
+++ views/links.php
@@ -3,5 +3,6 @@
 <link rel="stylesheet" type="text/css" href="/css/reset.css">
 <!-- Bootstrap CDN -->
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/css/css/hover-min.css">
 <link rel="stylesheet" type="text/css" href="/css/main.css">
 <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js?ver=1.4.2'></script>
